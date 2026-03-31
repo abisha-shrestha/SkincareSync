3e54e63885d3c9ebf1631c58efa510662bb4f028
abisha-shrestha <abishastha113@gmail.com>
2026-03-31 08:14:19 +0545

    feat(profile): persist skin type quiz results in user profile

--- backend/Routes/ProfileRouter.js
+++ backend/Routes/ProfileRouter.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { getProfile, updateProfile, changePassword, deleteAccount } = require('../Controllers/ProfileController');
+const { getProfile, updateProfile, changePassword, deleteAccount, saveSkinType, getSkinType } = require('../Controllers/ProfileController');
 
 router.get('/', getProfile);
 router.put('/', updateProfile);
+router.get('/skin-type', getSkinType);
+router.put('/skin-type', saveSkinType);
 router.put('/change-password', changePassword);
 router.put('/delete-account', deleteAccount);
 
